749718c6dd83b3ad45071c7abafec617e6fad420
keonhyeonwoo <khw19980926@gmail.com>
2026-04-17 17:52:14 +0900

    router 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { Outlet, Route, Routes } from "react-router";
 import ShadcnPoint from "./shadcn/ShadcnPoint";
 import { Toaster } from "sonner";
+import IndexPage from "@/pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      auth layout
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-      <ShadcnPoint />
-      <Toaster />
-    </>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
