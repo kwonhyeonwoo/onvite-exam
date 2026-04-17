e2d03692e24c98bf4f75d1c564d0018621ea60d6
keonhyeonwoo <khw19980926@gmail.com>
2026-04-17 17:33:26 +0900

    AlertDialog, Dialog, Carousel 정리

--- src/shadcn/ShadcnPoint.tsx
+++ src/shadcn/ShadcnPoint.tsx
@@ -1,10 +1,85 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
 const ShadcnPoint = () => {
   return (
     <div className="flex flex-col gap-y-8 p-10">
+      {/* Alert Dialog */}
+      <AlertDialog>
+        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Alert Dialog Title</AlertDialogTitle>
+            <AlertDialogDescription>
+              Alert Dialog Description
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/* Dialog 모달 창 */}
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogDescription>Dialog Description</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      {/* carousel -> 슬라이드 컴포넌트  */}
+      <Carousel className="m-4">
+        <CarouselContent>
+          <CarouselItem className="flex justify-center bg-red-500 text-white">
+            1
+          </CarouselItem>
+          <CarouselItem>2</CarouselItem>
+          <CarouselItem>3</CarouselItem>
+          <CarouselItem>4</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
       {/* 버튼 디자인모음, variant로 디자인 변경 가능 */}
       <div>
         <Button
